e2639133bd1da4c5155263ba67d46055cc7d4732
Jun Joy <chojuneseo1@gmail.com>
2021-07-12 18:34:13 +0900

    UPDATE; animation-module

--- Introduction/src/components/Hamburger/Hamburger.tsx
+++ Introduction/src/components/Hamburger/Hamburger.tsx
@@ -1,94 +1,77 @@
 import * as S from "./styles";
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import gsap from "gsap";
-import { hoverAnimation, hoverExitAnimation } from "./animations";
+import {
+  hoverAnimation,
+  hoverExitAnimation,
+  closeHamburgerMenuBackground,
+  closeHamburgerMenu,
+  closeMenuLinks,
+  revealMenuLinks,
+  revealHamburgerMenu,
+  revealHamburgerMenuBackground,
+  revealHamburgerMenuBackgroundSetting,
+  revealNavBar,
+} from "./animations";
 
 const Hamburger = ({ state }: any): JSX.Element => {
   let menu = useRef<any>(null);
   let revealMenu = useRef(null);
-  let revealMenuBackground = useRef(null);
+  let revealMenuBackground = useRef<any>(null);
   let menuLinks = useRef<any>(null);
   let nav1 = useRef<any>(null);
   let nav2 = useRef<any>(null);
-  let nav3 = useRef(null);
+  let nav3 = useRef<any>(null);
 
   useEffect(() => {
     if (state.clicked === false) {
-      gsap.to(menuLinks.current, {
-        delay: 0,
-        css: {display: "none"}
-      })   
-      gsap.to([revealMenu.current, revealMenuBackground.current], {
-        delay: .1,
-        duration: 0.8,
-        height: 0,
-        ease: 'power3.inOut',
-        stagger: {
-          amount: 0.06
-        }
-      });
-      gsap.to(menu.current, {
-        duration: 1,
-        css: {display: "none"}
-      })
-        
+      closeMenuLinks(menuLinks.current);
+      closeHamburgerMenuBackground(revealMenu.current, revealMenuBackground.current);
+      closeHamburgerMenu(menu.current);
     } else if (
       state.clicked == true ||
       (state.clicked == true && state.initial === null)
     ) {
-      gsap.to(menuLinks.current, {
-        delay: 0,
-        css: {display: "block"}
-      })  
-      gsap.to(menu.current, {
-        duration: 0,
-        css: { display: "block" },
-      });
-      gsap.to([revealMenuBackground.current, revealMenu.current], {
-        duration: 0,
-        delay: 0,
-        opacity: 1,
-        height: "100%",
-        
-      });
-      gsap.from([revealMenuBackground.current, revealMenu.current], {
-        duration: 0.8,
-        height: 0,
-        transformOrigin: "right top",
-        ease: "power3.inOut",
-        skewY: 3,
-        stagger: {
-          amount: 0.06,
-        },
-      });
-      gsap.from([nav1.current, nav2.current, nav3.current], {
-        duration: 0.8,
-        y: 100,
-        delay: 0.1,
-        height: 0,
-        ease: 'power3.inOut',
-        stagger: {
-          amount: 0.3
-        }
-      })
+      revealMenuLinks(menuLinks.current);
+      revealHamburgerMenu(menu.current);
+      revealHamburgerMenuBackgroundSetting(revealMenuBackground.current, revealMenu.current);
+      revealHamburgerMenuBackground(revealMenuBackground.current, revealMenu.current);
+      revealNavBar(nav1.current, nav2.current, nav3.current);
     }
   }, [state]);
-  
+
   return (
     <S.HamburgerMenu ref={menu}>
       <S.MenuSecondaryBackgroundColor ref={revealMenuBackground} />
       <S.MenuLayer ref={revealMenu}>
         <S.MenuLinks ref={menuLinks}>
           <S.UnorderedList>
-            <S.List ref={nav1} >
-              <Link to="/introduction" onMouseOver={e => hoverAnimation(e)} onMouseOut={e => hoverExitAnimation(e)}>Introduction</Link>
+            <S.List ref={nav1}>
+              <Link
+                to="/introduction"
+                onMouseOver={(e) => hoverAnimation(e)}
+                onMouseOut={(e) => hoverExitAnimation(e)}
+              >
+                Introduction
+              </Link>
             </S.List>
             <S.List ref={nav2}>
-              <Link to="/projects" onMouseOver={e => hoverAnimation(e)} onMouseOut={e => hoverExitAnimation(e)}>Projects</Link>
+              <Link
+                to="/projects"
+                onMouseOver={(e) => hoverAnimation(e)}
+                onMouseOut={(e) => hoverExitAnimation(e)}
+              >
+                Projects
+              </Link>
             </S.List>
-            <S.List ref={nav3} >
-              <Link to="/contactus" onMouseOver={e => hoverAnimation(e)} onMouseOut={e => hoverExitAnimation(e)}>Contact us</Link>
+            <S.List ref={nav3}>
+              <Link
+                to="/contactus"
+                onMouseOver={(e) => hoverAnimation(e)}
+                onMouseOut={(e) => hoverExitAnimation(e)}
+              >
+                Contact us
+              </Link>
             </S.List>
           </S.UnorderedList>
         </S.MenuLinks>
--- Introduction/src/components/Hamburger/animations.tsx
+++ Introduction/src/components/Hamburger/animations.tsx
@@ -1,7 +1,85 @@
-import gsap from "gsap";
+import { gsap, TweenLite } from "gsap";
+
+export const closeMenuLinks = (node: any) => {
+  TweenLite.to(node, {
+    delay: 0,
+    css: { display: "none" },
+  });
+};
+
+export const closeHamburgerMenuBackground = (node1: any, node2: any) => {
+  TweenLite.to([node1, node2], {
+    delay: 0.1,
+    duration: 0.8,
+    height: 0,
+    ease: "power3.inOut",
+    stagger: {
+      amount: 0.06,
+    },
+  });
+};
+
+export const closeHamburgerMenu = (node: any) => {
+  TweenLite.to(node, {
+    duration: 1,
+    css: { display: "none" },
+  });
+};
+
+export const revealMenuLinks = (node: any) => {
+  TweenLite.to(node, {
+    delay: 0,
+    css: { display: "block" },
+  });
+};
+
+export const revealHamburgerMenu = (node: any) => {
+  TweenLite.to(node, {
+    duration: 0,
+    css: { display: "block" },
+  });
+};
+
+export const revealHamburgerMenuBackgroundSetting = (
+  node1: any,
+  node2: any
+) => {
+  TweenLite.to([node1, node2], {
+    duration: 0,
+    delay: 0,
+    opacity: 1,
+    height: "100%",
+  });
+};
+
+export const revealHamburgerMenuBackground = (node1: any, node2: any) => {
+  TweenLite.from([node1, node2], {
+    duration: 0.8,
+    height: 0,
+    transformOrigin: "right top",
+    ease: "power3.inOut",
+    skewY: 3,
+    stagger: {
+      amount: 0.06,
+    },
+  });
+};
+
+export const revealNavBar = (node1: any, node2: any, node3: any) => {
+  TweenLite.from([node1, node2, node3], {
+    duration: 0.8,
+    y: 100,
+    delay: 0.1,
+    height: 0,
+    ease: "power3.inOut",
+    stagger: {
+      amount: 0.3,
+    },
+  });
+};
 
 export const hoverAnimation = (e: any) => {
-  gsap.to(e.target.parentNode, {
+  TweenLite.to(e.target.parentNode, {
     duration: 0.1,
     y: 3,
     skewX: 4,
@@ -10,7 +88,7 @@ export const hoverAnimation = (e: any) => {
 };
 
 export const hoverExitAnimation = (e: any) => {
-  gsap.to(e.target.parentNode, {
+  TweenLite.to(e.target.parentNode, {
     duration: 0.1,
     y: -3,
     skewX: 0,
